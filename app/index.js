d076f66c0c212bae6c83352492feab4d4d9676bb
Razafintsialonina Mamy <nyandrianinamamy@gmail.com>
2019-11-06 18:03:35 +0300

    [APP] move folders to root directory #master

--- app/index.js
+++ app/index.js
@@ -3,6 +3,7 @@ var Generator = require('yeoman-generator');
 module.exports = class extends Generator {
     constructor(args, opts) {
         super(args, opts);
-        this.log('Not implemented use feature');
+        this.argument('appName', { type: String, required: true });
+        this.appName = this.options.appName;
     }
 };
