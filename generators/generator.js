a52defac987d6d7ddca3f5d626d5b73af71adbb5
Razafintsialonina Mamy <nyandrianinamamy@gmail.com>
2019-11-06 17:48:38 +0300

    [GENERATOR] add basePath name #master

--- generators/generator.js
+++ generators/generator.js
@@ -2,28 +2,35 @@ const underscore = require('underscore.string');
 const Generator = require('yeoman-generator');
 
 module.exports = class extends Generator {
-    constructor(args, opts, layer) {
+    constructor(args, opts, layer, files) {
         super(args, opts);
         this.argument('name', { type: String, required: true });
         this.layer = layer;
+        this.files = files || [];
+        this.files.push(`${this.layer}.ts`);
     }
 
     writing() {
-        const files = ['index.ts', `${this.layer}.ts`];
         const strings = this._stringHelpers(this.options.name);
         const copyOpts = {
             globOptions: {
                 ignore: [],
             },
         };
-        files.forEach((file) => {
+        this.files.forEach((file) => {
+            let destinationPath = '';
+            // add base app name
+            if (this.options.appName) {
+                destinationPath = `${this.options.appName}/`;
+            }
+            // file name and directory tree
+            destinationPath +=
+                file !== 'index.ts'
+                    ? `${strings.dash}/${this.layer}/${strings.dash}.${file}`
+                    : `${strings.dash}/${this.layer}/${file}`;
             this.fs.copyTpl(
                 this.templatePath(`${this.layer}/${file}`),
-                this.destinationPath(
-                    file !== 'index.ts'
-                        ? `${strings.dash}/${this.layer}/${strings.dash}.${file}`
-                        : `${strings.dash}/${this.layer}/${file}`,
-                ),
+                this.destinationPath(destinationPath),
                 strings,
                 copyOpts,
             );
