cd5e4708062d1f4986ed07eebe6db9db0ab2e0a0
Razafintsialonina Mamy <nyandrianinamamy@gmail.com>
2019-11-06 15:42:52 +0300

    [APP] initial commit #master

---
+++ generators/app/index.js
@@ -0,0 +1,8 @@
+var Generator = require('yeoman-generator');
+
+module.exports = class extends Generator {
+    constructor(args, opts) {
+        super(args, opts);
+        this.log('Not implemented use feature');
+    }
+};
