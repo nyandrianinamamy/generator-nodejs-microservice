3efe315fe0cd54bd1dbeac8512158ab72333e919
Razafintsialonina Mamy <nyandrianinamamy@gmail.com>
2019-11-20 16:46:37 +0300

    [ENTITY] add description field #master

--- entity/templates/entity/entity.ts
+++ entity/templates/entity/entity.ts
@@ -1,9 +1,10 @@
 export class <%= pascal %>Builder {
-    constructor() {}
-
     make<%= pascal %>(<%= camel %>: I<%= pascal %>) {
         if (!<%= camel %>.name) {
             throw new Error('<%= pascal %> must have a name.');
+        }
+        if (!<%= camel %>.description) {
+            throw new Error('<%= pascal %> must have a description.');
         }
         return {
             ...<%= camel %>
@@ -15,4 +16,7 @@ export interface I<%= pascal %> {
     // tslint:disable: no-any member-ordering
     _id: any;
     name: string;
+    description: string;
+    createdAt: Date;
+    updatedAt: Date;
 }
