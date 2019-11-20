4decd269bb8b21f32283bc816003741806dba13a
Razafintsialonina Mamy <nyandrianinamamy@gmail.com>
2019-11-20 16:49:46 +0300

    [ENTITY]  add text index, timestamps #master

--- entity/templates/entity/index.ts
+++ entity/templates/entity/index.ts
@@ -1,5 +1,8 @@
+import { Model, model } from 'mongoose';
 import { <%= pascal %>Builder } from './<%= dash %>.entity';
+import { I<%= pascal %>Model, <%= camel %>Schema } from './<%= dash %>.mongoose.schema';
 
 const <%= camel %>Builder = new <%= pascal %>Builder();
+const <%= camel %>Model: Model<I<%= pascal %>Model> = model<I<%= pascal %>Model>('<%= pascal %>', <%= camel %>Schema);
 
-export { <%= camel %>Builder };
+export { <%= camel %>Builder, <%= camel %>Model };
--- entity/templates/entity/mongoose.schema.ts
+++ entity/templates/entity/mongoose.schema.ts
@@ -2,11 +2,14 @@ import { Document, Model, model, Schema } from 'mongoose';
 import { I<%= pascal %> } from './<%= dash %>.entity';
 
 export interface I<%= pascal %>Model extends I<%= pascal %>, Document {}
-export const <%= camel %>Schema: Schema<I<%= pascal %>> = new Schema<I<%= pascal %>>({
-    name: String
-});
-
-export const <%= camel %>Model: Model<I<%= pascal %>Model> = model<I<%= pascal %>Model>(
-    '<%= pascal %>',
-    <%= camel %>Schema,
+export const <%= camel %>Schema: Schema<I<%= pascal %>> = new Schema<I<%= pascal %>>(
+    {
+        name: { type: String, required: true, trim: true },
+        description: { type: String, required: true },
+    },
+    { timestamps: true }
 );
+
+const textIndex = { name: 'text', description: 'text' };
+
+<%= camel %>Schema.index(textIndex);
